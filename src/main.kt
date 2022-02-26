d85c25f7673e4fa2987e4a6950f09ce88d0c414a
SalmaSafe <salmaasulee@gmail.com>
2022-02-26 17:39:04 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,21 @@
+fun main(){
+    var school = "AkiraChix"
+    var result =school[0].toString()+ school[2] + school[3]
+    println(result)
+
+    var X = "Salma"
+    var Y = "21"
+    var results ="Hi, my name is $X and I am $Y years old"
+    println(results)
+
+    var text = "Ribashongilogasheshiakili"
+    println("length of the text String is: "+ text.length)
+
+    var student="Salma"
+    if(student=="Salma"){
+        println("That's me")
+    }
+    else{
+        println("I don't know who that is")
+    }
+}
